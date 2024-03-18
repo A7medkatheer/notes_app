db433e4ecfbc7a02388a43fecb0d77a53e4a1724
ahmed katheer <UGS.141174@ci.suez.edu.eg>
2024-03-19 00:07:53 +0200

    take input and validate data

--- lib/views/widgets/add_notes_botton_sheet.dart
+++ lib/views/widgets/add_notes_botton_sheet.dart
@@ -11,30 +11,70 @@ class AddNotesBottonSheet extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 32,
-            ),
-            CustomTextField(
-              hintText: "add Title",
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            CustomTextField(
-              hintText: "content",
-              maxLines: 5,
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            CustomButton(),
-            SizedBox(
-              height: 32,
-            ),
-          ],
-        ),
+        child: AddNoteForm(),
+      ),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+final GlobalKey<FormState> formKey = GlobalKey();
+AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+String? title, subTitle;
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            onsave: (value) {
+              title = value;
+            },
+            hintText: "add Title",
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onsave: (value) {
+              subTitle = value;
+            },
+            hintText: "content",
+            maxLines: 5,
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          CustomButton(
+            ontap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+                Navigator.pop(context, [title, subTitle]);
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+        ],
       ),
     );
   }
