9a9f1dc2a0d60606180659b66c6ad016eec9c921
ahmed katheer <UGS.141174@ci.suez.edu.eg>
2024-03-21 13:56:29 +0200

    create notes cubit

---
+++ lib/notes_cubit/notes_cubit_state.dart
@@ -0,0 +1,15 @@
+part of 'notes_cubit_cubit.dart';
+
+@immutable
+abstract class NotesCubitState {}
+
+ class NotesCubitInitial extends NotesCubitState {}
+ class NotesLoading extends NotesCubitState {}
+ class NotesSuccess extends NotesCubitState {
+    final List<NotesModel> notes;
+    NotesSuccess(this.notes);
+ }
+ class NotesFailure extends NotesCubitState {
+    final String errMessage;
+    NotesFailure(this.errMessage);
+ }
